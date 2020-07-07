4413db51dfb250fe172b6cbc3a56f2d8a91c504d
Saugat Shrestha <saugatshrestha@Saugats-MacBook-Pro.local>
2020-07-07 11:38:31 -0500

    second commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import styles from "./App.css";
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.hhh}>
+        <Layout>
+          <BurgerBuilder></BurgerBuilder>
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
